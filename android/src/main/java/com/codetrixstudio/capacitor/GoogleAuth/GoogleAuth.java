696d8d8d1f23d0838dd6c7916d9c1bbad7b0320a
Kostas Kayioulis <kostas@kayioulis.com>
2021-09-13 03:57:29 +0300

    Retrieve access token on android & update GoogleSignIn Pod to 6.0.2

--- android/src/main/java/com/codetrixstudio/capacitor/GoogleAuth/GoogleAuth.java
+++ android/src/main/java/com/codetrixstudio/capacitor/GoogleAuth/GoogleAuth.java
@@ -1,7 +1,13 @@
 package com.codetrixstudio.capacitor.GoogleAuth;
 
+import android.accounts.Account;
+import android.accounts.AccountManager;
+import android.accounts.AccountManagerFuture;
 import android.content.Intent;
 import androidx.activity.result.ActivityResult;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.util.Log;
 
 import com.codetrixstudio.capacitor.GoogleAuth.capacitorgoogleauth.R;
 import com.getcapacitor.JSObject;
@@ -20,9 +26,24 @@ import com.google.android.gms.tasks.Task;
 
 import org.json.JSONArray;
 import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedInputStream;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
 
 @CapacitorPlugin()
 public class GoogleAuth extends Plugin {
+  private final static String VERIFY_TOKEN_URL        = "https://www.googleapis.com/oauth2/v1/tokeninfo?access_token=";
+  private final static String FIELD_TOKEN_EXPIRES_IN  = "expires_in";
+  private final static String FIELD_ACCESS_TOKEN      = "accessToken";
+  private final static String FIELD_TOKEN_EXPIRES     = "expires";
+  public static final int KAssumeStaleTokenSec = 60;
+
   private GoogleSignInClient googleSignInClient;
 
   @Override
@@ -35,11 +56,9 @@ public class GoogleAuth extends Plugin {
 
     GoogleSignInOptions.Builder googleSignInBuilder = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
             .requestIdToken(clientId)
+            .requestServerAuthCode(clientId, forceCodeForRefreshToken)
             .requestEmail();
 
-    if (forceCodeForRefreshToken) {
-      googleSignInBuilder.requestServerAuthCode(clientId, true);
-    }
 
     String[] scopeArray = getConfig().getArray("scopes", new String[] {});
     Scope[] scopes = new Scope[scopeArray.length - 1];
@@ -67,27 +86,107 @@ public class GoogleAuth extends Plugin {
     Task<GoogleSignInAccount> completedTask = GoogleSignIn.getSignedInAccountFromIntent(result.getData());
 
     try {
-      GoogleSignInAccount account = completedTask.getResult(ApiException.class);
+      final GoogleSignInAccount account = completedTask.getResult(ApiException.class);
 
-      JSObject authentication = new JSObject();
-      authentication.put("idToken", account.getIdToken());
+      new AsyncTask<Void, Void, Void>() {
+        @Override
+        protected Void doInBackground(Void... params) {
 
-      JSObject user = new JSObject();
-      user.put("serverAuthCode", account.getServerAuthCode());
-      user.put("idToken", account.getIdToken());
-      user.put("authentication", authentication);
+          JSONObject result = new JSONObject();
+          try {
+            JSONObject accessTokenObject = getAuthToken(account.getAccount(), true);
 
-      user.put("displayName", account.getDisplayName());
-      user.put("email", account.getEmail());
-      user.put("familyName", account.getFamilyName());
-      user.put("givenName", account.getGivenName());
-      user.put("id", account.getId());
-      user.put("imageUrl", account.getPhotoUrl());
+            JSObject authentication = new JSObject();
+            authentication.put("idToken", account.getIdToken());
+            authentication.put(FIELD_ACCESS_TOKEN, accessTokenObject.get(FIELD_ACCESS_TOKEN));
+            authentication.put(FIELD_TOKEN_EXPIRES, accessTokenObject.get(FIELD_TOKEN_EXPIRES));
+            authentication.put(FIELD_TOKEN_EXPIRES_IN, accessTokenObject.get(FIELD_TOKEN_EXPIRES_IN));
 
-      call.resolve(user);
+            JSObject user = new JSObject();
+            user.put("serverAuthCode", account.getServerAuthCode());
+            user.put("idToken", account.getIdToken());
+            user.put("authentication", authentication);
+
+            user.put("displayName", account.getDisplayName());
+            user.put("email", account.getEmail());
+            user.put("familyName", account.getFamilyName());
+            user.put("givenName", account.getGivenName());
+            user.put("id", account.getId());
+            user.put("imageUrl", account.getPhotoUrl());
+
+            call.resolve(user);
+          } catch (Exception e) {
+            e.printStackTrace();
+            call.reject("Something went wrong when retrieve access token", e);
+          }
+          return null;
+        }
+      }.execute();
     } catch (ApiException e) {
       call.reject("Something went wrong", e);
     }
+  }
+
+  private JSONObject getAuthToken(Account account, boolean retry) throws Exception {
+    AccountManager manager = AccountManager.get(getContext());
+    AccountManagerFuture<Bundle> future = manager.getAuthToken (account, "oauth2:profile email", null, false, null, null);
+    Bundle bundle = future.getResult();
+    String authToken = bundle.getString(AccountManager.KEY_AUTHTOKEN);
+    try {
+      return verifyToken(authToken);
+    } catch (IOException e) {
+      if (retry) {
+        manager.invalidateAuthToken("com.google", authToken);
+        return getAuthToken(account, false);
+      } else {
+        throw e;
+      }
+    }
+  }
+
+  private JSONObject verifyToken(String authToken) throws IOException, JSONException {
+    URL url = new URL(VERIFY_TOKEN_URL+authToken);
+    HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
+    urlConnection.setInstanceFollowRedirects(true);
+    String stringResponse = fromStream(
+            new BufferedInputStream(urlConnection.getInputStream())
+    );
+        /* expecting:
+        {
+          "issued_to": "xxxxxx-xxxxxxxxxxxxxxx.apps.googleusercontent.com",
+          "audience": "xxxxxx-xxxxxxxxxxxxxxxx.apps.googleusercontent.com",
+          "user_id": "xxxxxxxxxxxxxxxxxxxx",
+          "scope": "https://www.googleapis.com/auth/userinfo.email openid https://www.googleapis.com/auth/userinfo.profile",
+          "expires_in": 3220,
+          "email": "xxxxxxx@xxxxx.com",
+          "verified_email": true,
+          "access_type": "online"
+         }
+        */
+
+    Log.d("AuthenticatedBackend", "token: " + authToken + ", verification: " + stringResponse);
+    JSONObject jsonResponse = new JSONObject(
+            stringResponse
+    );
+    int expires_in = jsonResponse.getInt(FIELD_TOKEN_EXPIRES_IN);
+    if (expires_in < KAssumeStaleTokenSec) {
+      throw new IOException("Auth token soon expiring.");
+    }
+    jsonResponse.put(FIELD_ACCESS_TOKEN, authToken);
+    jsonResponse.put(FIELD_TOKEN_EXPIRES, expires_in + (System.currentTimeMillis()/1000));
+    return jsonResponse;
+  }
+
+  public static String fromStream(InputStream is) throws IOException {
+    BufferedReader reader = new BufferedReader(
+            new InputStreamReader(is));
+    StringBuilder sb = new StringBuilder();
+    String line = null;
+    while ((line = reader.readLine()) != null) {
+      sb.append(line).append("\n");
+    }
+    reader.close();
+    return sb.toString();
   }
 
   @PluginMethod()
